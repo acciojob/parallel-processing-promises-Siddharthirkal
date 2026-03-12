cb062eb4049ab331cdc870742fe325426ad38d4b
Siddharthirkal <104129670+Siddharthirkal@users.noreply.github.com>
2026-03-12 16:19:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,53 +2,40 @@ document.addEventListener("DOMContentLoaded", () => {
 
 const output = document.getElementById("output");
 const btn = document.getElementById("download-images-button");
-const errorDiv = document.getElementById("error");
-const loadingDiv = document.getElementById("loading");
 
 const images = [
   { url: "https://picsum.photos/id/237/200/300" },
   { url: "https://picsum.photos/id/238/200/300" },
-  { url: "https://picsum.photos/id/239/200/300" },
+  { url: "https://picsum.photos/id/239/200/300" }
 ];
 
 function downloadImage(url){
-	return new Promise((resolve, reject) => {
+  return new Promise((resolve,reject)=>{
 
-		const img = document.createElement("img");
-		img.src = url;
+    const img = document.createElement("img");
+    img.src = url;
 
-		img.onload = () => resolve(img);
-		img.onerror = () => reject("Failed to load image: " + url);
+    img.onload = ()=> resolve(img);
+    img.onerror = ()=> reject("Image failed");
 
-	});
+  });
 }
 
 function downloadImages(){
 
-	output.innerHTML = "";
-	errorDiv.textContent = "";
+  const promises = images.map(img => downloadImage(img.url));
 
-	loadingDiv.style.display = "block";
+  Promise.all(promises)
+    .then(result => {
 
-	const promises = images.map(image => downloadImage(image.url));
+      result.forEach(img=>{
+        output.appendChild(img);
+      });
 
-	Promise.all(promises)
-	.then((imgs)=>{
-
-		loadingDiv.style.display = "none";
-
-		imgs.forEach(img=>{
-			output.appendChild(img);
-		});
-
-	})
-	.catch((error)=>{
-
-		loadingDiv.style.display = "none";
-		errorDiv.textContent = error;
-
-	});
-
+    })
+    .catch(err=>{
+      console.log(err);
+    });
 }
 
 btn.addEventListener("click", downloadImages);
